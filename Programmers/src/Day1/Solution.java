d93f82dc886608a24cb49d98d3cb246aeca28912
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-27 16:31:57 +0900

    Day1

---
+++ Programmers/src/Day1/Solution.java
@@ -0,0 +1,13 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        System.out.println(str); 
+    }
+}
